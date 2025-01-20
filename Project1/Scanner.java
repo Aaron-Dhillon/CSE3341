35412a77f94a31e579895ef98c19afece8f40b4c
Aaron-Dhillon <adhillonschool@gmail.com>
2025-01-20 01:04:16 -0500

    Initialize Keywords/Symbols

--- Project1/Scanner.java
+++ Project1/Scanner.java
@@ -1,7 +1,11 @@
 import java.io.*;
+import java.util.*;
 class Scanner {
     BufferedReader bReader;
     FileReader fReader;
+    private Map<String, Core> keywords = new HashMap<String, Core>();
+    private Map<Character, Core> symbols = new HashMap<Character, Core>();
+
     // Initialize the scanner
     Scanner(String filename) {
         try{
@@ -11,6 +15,45 @@ class Scanner {
             System.out.println("File not found");
             System.exit(1);
         }
+
+        // Initialize the keywords and symbols
+        keywords.put("and", Core.AND);
+        keywords.put("begin", Core.BEGIN);
+        keywords.put("case", Core.CASE);
+        keywords.put("do", Core.DO);
+        keywords.put("else", Core.ELSE);
+        keywords.put("end", Core.END);
+        keywords.put("for", Core.FOR);
+        keywords.put("if", Core.IF);
+        keywords.put("in", Core.IN);
+        keywords.put("integer", Core.INTEGER);
+        keywords.put("is", Core.IS);
+        keywords.put("new", Core.NEW);
+        keywords.put("not", Core.NOT);
+        keywords.put("object", Core.OBJECT);
+        keywords.put("or", Core.OR);
+        keywords.put("print", Core.PRINT);
+        keywords.put("procedure", Core.PROCEDURE);
+        keywords.put("read", Core.READ);
+        keywords.put("return", Core.RETURN);
+        keywords.put("then", Core.THEN);
+
+        symbols.put('+', Core.ADD);
+        symbols.put('-', Core.SUBTRACT);
+        symbols.put('*', Core.MULTIPLY);
+        symbols.put('/', Core.DIVIDE);
+        symbols.put('=', Core.ASSIGN);
+        symbols.put('<', Core.LESS);
+        symbols.put(':', Core.COLON);
+        symbols.put(';', Core.SEMICOLON);
+        symbols.put('.', Core.PERIOD);
+        symbols.put(',', Core.COMMA);
+        symbols.put('(', Core.LPAREN);
+        symbols.put(')', Core.RPAREN);
+        symbols.put('[', Core.LSQUARE);
+        symbols.put(']', Core.RSQUARE);
+        symbols.put('{', Core.LCURL);
+        symbols.put('}', Core.RCURL);
 
     }
 
@@ -31,12 +74,13 @@ class Scanner {
 
 	// Return the constant value
     public int getConst() {
-        return null;
+        return 0;
     }
 	
 	// Return the character string
     public String getString() {
         return null;
     }
+    
 
 }
