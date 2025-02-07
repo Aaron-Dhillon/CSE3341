4c754678d2236344202752c369669f0b185a9907
Aaron-Dhillon <adhillonschool@gmail.com>
2025-02-07 00:34:57 -0500

    All files created and example code created

--- Project2/Main.java
+++ Project2/Main.java
@@ -1,26 +1,15 @@
+import java.util.Scanner;
+
 class Main {
 	public static void main(String[] args) {
-		// Initialize the scanner with the input file
+		//Initialize the scanner witht the input file
 		Scanner S = new Scanner(args[0]);
+		Parser.scanner = S;
 
-		// Print the token stream
-		while (S.currentToken() != Core.EOS && S.currentToken() != Core.ERROR) {
-			// Pring the current token, with any extra data needed
-			System.out.print(S.currentToken());
-			if (S.currentToken() == Core.ID) {
-				String value = S.getId();
-				System.out.print("[" + value + "]");
-			} else if (S.currentToken() == Core.CONST) {
-				int value = S.getConst();
-				System.out.print("[" + value + "]");
-			} else if (S.currentToken() == Core.STRING) {
-				String value = S.getString();
-				System.out.print("[" + value + "]");
-			}
-			System.out.print("\n");
+		Procedure p = new Procedure();
 
-			// Advance to the next token
-			S.nextToken();
-		}
-	}
+		p.parse();
+
+		p.print();
+	}	
 }
