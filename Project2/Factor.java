0a287f63121c489f54dd9e049570f07270a59e3f
Aaron-Dhillon <adhillonschool@gmail.com>
2025-02-07 01:46:07 -0500

    Factor can possibly have string indexing

--- Project2/Factor.java
+++ Project2/Factor.java
@@ -2,13 +2,29 @@ class Factor {
     String id;
     int constant;
     Expr expr;
+    String stringIndex; // Ensures only a string inside id[string]
     boolean isConst = false;
     boolean isExpr = false;
+    boolean isArrayAccess = false;
 
     void parse() {
         if (Parser.scanner.currentToken() == Core.ID) {
             id = Parser.scanner.getId();
             Parser.scanner.nextToken();
+
+            // Check if it's an array access: id[string]
+            if (Parser.scanner.currentToken() == Core.LSQUARE) {
+                isArrayAccess = true;
+                Parser.scanner.nextToken(); // Consume '['
+
+                // Expect only a STRING inside the brackets
+                Parser.expectedToken(Core.STRING);
+                stringIndex = Parser.scanner.getString();
+                Parser.scanner.nextToken(); // Consume the string
+
+                Parser.expectedToken(Core.RSQUARE);
+                Parser.scanner.nextToken(); // Consume ']'
+            }
         } else if (Parser.scanner.currentToken() == Core.CONST) {
             constant = Parser.scanner.getConst();
             isConst = true;
@@ -21,13 +37,14 @@ class Factor {
             Parser.expectedToken(Core.RPAREN);
             Parser.scanner.nextToken();
         } else {
-            System.out.println("ERROR: Invalid Factor");
-            System.exit(1);
+            throw new RuntimeException("ERROR: Invalid Factor - Unexpected token " + Parser.scanner.currentToken());
         }
     }
 
     void print() {
-        if (id != null) {
+        if (isArrayAccess) {
+            System.out.print(id + "[" + "\"" + stringIndex + "\"" + "]");
+        } else if (id != null) {
             System.out.print(id);
         } else if (isConst) {
             System.out.print(constant);
