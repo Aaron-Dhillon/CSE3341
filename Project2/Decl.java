30da24915352d41526050c163a834ea60eb92c98
Aaron-Dhillon <adhillonschool@gmail.com>
2025-02-07 16:29:18 -0500

    Decl and Stmt fix

--- Project2/Decl.java
+++ Project2/Decl.java
@@ -1,20 +1,36 @@
 public class Decl {
     String id;
-    
+    boolean isInteger; // Determines whether it's an integer or object declaration
+
     void parse() {
+        // Expect 'integer' or 'object'
+        if (Parser.scanner.currentToken() == Core.INTEGER) {
+            isInteger = true;
+        } else if (Parser.scanner.currentToken() == Core.OBJECT) {
+            isInteger = false;
+        } else {
+            System.out.println("ERROR: Expected 'integer' or 'object' in declaration.");
+            System.exit(1);
+        }
+        Parser.scanner.nextToken(); // Consume 'integer' or 'object'
+
+        // Expect ID
         Parser.expectedToken(Core.ID);
         id = Parser.scanner.getId();
         Parser.scanner.nextToken();
 
-        Parser.expectedToken(Core.COLON);
-        Parser.scanner.nextToken();
-    
-        Parser.expectedToken(Core.INTEGER);
+        // Expect ';'
+        Parser.expectedToken(Core.SEMICOLON);
         Parser.scanner.nextToken();
     }
-    
+
     void print() {
-        System.out.println(id + " : integer;");
+        if (isInteger) {
+            System.out.println("integer " + id + ";");
+        } else {
+            System.out.println("object " + id + ";");
+        }
     }
 }
+
     
--- Project2/Stmt.java
+++ Project2/Stmt.java
@@ -1,22 +1,57 @@
 public class Stmt {
     Assign assign;
     Print printStmt;
+    If ifStmt;
+    Loop loopStmt;
+    Read readStmt;
+    Decl declStmt;
 
     void parse() {
-        if (Parser.scanner.currentToken() == Core.ID) {
-            assign = new Assign();
-            assign.parse();
-        } else if (Parser.scanner.currentToken() == Core.PRINT) {
-            printStmt = new Print();
-            printStmt.parse();
-        } else {
-            System.out.println("ERROR: Unexpected statement.");
-            System.exit(1);
+        switch (Parser.scanner.currentToken()) {
+            case ID:
+                // Assignment statement
+                assign = new Assign();
+                assign.parse();
+                break;
+            case PRINT:
+                // Print statement
+                printStmt = new Print();
+                printStmt.parse();
+                break;
+            case IF:
+                // If statement
+                ifStmt = new If();
+                ifStmt.parse();
+                break;
+            case FOR:
+                // Loop statement
+                loopStmt = new Loop();
+                loopStmt.parse();
+                break;
+            case READ:
+                // Read statement
+                readStmt = new Read();
+                readStmt.parse();
+                break;
+            case INTEGER:
+            case OBJECT:
+                // Declaration statement
+                declStmt = new Decl();
+                declStmt.parse();
+                break;
+            default:
+                // Error: unexpected token
+                System.out.println("ERROR: Unexpected statement.");
+                System.exit(1);
         }
     }
 
     void print() {
         if (assign != null) assign.print();
         if (printStmt != null) printStmt.print();
+        if (ifStmt != null) ifStmt.print();
+        if (loopStmt != null) loopStmt.print();
+        if (readStmt != null) readStmt.print();
+        if (declStmt != null) declStmt.print();
     }
 }
