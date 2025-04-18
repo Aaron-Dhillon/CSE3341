803df1083b268b1a1509c87d9394d013a13718c9
Aaron-Dhillon <adhillonschool@gmail.com>
2025-04-18 18:09:09 -0400

    cleaned up

--- Project6/myfns.scm
+++ Project6/myfns.scm
@@ -5,31 +5,15 @@
 ; Evaluates a PLAN expression with the current environment
 (define (evalExpr expr env)
   (cond
-    ; Case 1: Expression is an integer constant
     ((integer? expr) expr)
-    
-    ; Case 2: Expression is an identifier
     ((symbol? expr) (lookup expr env))
-    
-    ; Case 3: Expression is a list (compound expression)
     ((list? expr)
      (cond
-       ; planIf expression
        ((equal? (car expr) 'planIf) (evalPlanIf expr env))
-       
-       ; planAdd expression
        ((equal? (car expr) 'planAdd) (evalPlanAdd expr env))
-       
-       ; planMul expression
        ((equal? (car expr) 'planMul) (evalPlanMul expr env))
-       
-       ; planSub expression
        ((equal? (car expr) 'planSub) (evalPlanSub expr env))
-       
-       ; planLet expression
        ((equal? (car expr) 'planLet) (evalPlanLet expr env))
-       
-       ; Function call expression (id expr)
        (else (evalFunctionCall expr env))))))
 
 ; Evaluates a planIf expression
@@ -57,11 +41,8 @@
 (define (evalPlanLet expr env)
   (let ((id (cadr expr))
         (val-expr (caddr expr)))
-    ; Check if val-expr is a planFunction
     (if (and (list? val-expr) (equal? (car val-expr) 'planFunction))
-        ; Handle function definition
         (evalExpr (cadddr expr) (cons (cons id val-expr) env))
-        ; Handle regular variable binding
         (evalExpr (cadddr expr) (cons (cons id (evalExpr val-expr env)) env)))))
 
 ; Evaluates a function call expression
@@ -69,16 +50,12 @@
   (let ((func-id (car expr))
         (arg-expr (cadr expr)))
     (let ((func-def (lookup func-id env)))
-      (if (and (list? func-def) (equal? (car func-def) 'planFunction))
-          (let ((param-id (cadr func-def))
-                (body-expr (caddr func-def))
-                (arg-val (evalExpr arg-expr env)))
-            ; Create a new environment with the parameter binding
-            (evalExpr body-expr (cons (cons param-id arg-val) env)))
-          0)))) ; Should not happen with valid input
+      (let ((param-id (cadr func-def))
+            (body-expr (caddr func-def))
+            (arg-val (evalExpr arg-expr env)))
+        (evalExpr body-expr (cons (cons param-id arg-val) env))))))
 
 ; Looks up an identifier in the environment
-; Uses dynamic scoping (most recent binding)
 (define (lookup id env)
   (cond
     ((equal? (caar env) id) (cdar env))
